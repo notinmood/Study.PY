e067d9c25b159fae99037c78b91338242f1501e8
解大劦 <9727005@qq.com>
2024-03-06 07:54:09 +0800

    refactor: 重构目录

---
+++ F1.9.系统内置的生成器函数/1.py
@@ -0,0 +1,9 @@
+"""
+ * @file   : 1.py
+ * @time   : 7:46
+ * @date   : 2024/3/6
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+# TODO:xiedali@2024/03/06 学习内容 https://blog.csdn.net/max_LLL/article/details/124241937
