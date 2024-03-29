65afdb3618ff548d83d388c21d43431d8effb77c
解大劦 <develope@163.com>
2024-03-30 07:51:06 +0800

    feat: 删除无用的文件

---
+++ 各种格式文件的读写操作/TOML操作/0.使用内置的toml模块.py
@@ -0,0 +1,12 @@
+"""
+ * @file   : 0.使用内置的toml模块.py
+ * @time   : 7:09
+ * @date   : 2024/3/30
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+import tomllib
+
+if __name__ == '__main__':
+    tomllib.load("config.toml")
