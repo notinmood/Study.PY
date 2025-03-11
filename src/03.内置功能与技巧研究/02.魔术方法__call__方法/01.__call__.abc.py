47ccfeb5019a2c303e2e540fdf4fbabce4cac803
解大劦 <9727005@qq.com>
2025-03-11 09:54:56 +0800

    refactor: 调整目录结构

--- src/03.内置功能与技巧研究/02.魔术方法__call__方法/01.__call__.abc.py
+++ src/03.内置功能与技巧研究/02.魔术方法__call__方法/01.__call__.abc.py
@@ -19,6 +19,6 @@ class Demo:
 
 if __name__ == '__main__':
     # 创建对象实例的时候，会调用__init__方法
-    demo = Demo('aa')
+    demo = Demo('aa')  # 输出：我是在init里面的逻辑
     # 在实例上继续用()的时候，是调用的__call__方法（其他时候call不会被调用）
-    demo()
+    demo()  # 输出：我是在Call里面的逻辑
